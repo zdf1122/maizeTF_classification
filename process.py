8431d54bce4e42c0f62aff0e4714b6302de6f31e
zdf1122 <81453850+zdf1122@users.noreply.github.com>
2022-05-19 11:39:54 +0800

    Update process.py

--- process.py
+++ process.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 import random
-#选择三类转录因子数据
-kinds = [i for i in os.listdir('35个转录因子序列集') if i.split('.')[0] in
+#load data
+inds = [i for i in os.listdir('TFs') if i.split('.')[0] in
          ['bHLH_TF119','TCP_TF237','MYB_TF224']
          ]
 data = []
@@ -11,17 +11,17 @@ label = []
 c = 0
 for file in kinds:
     if file[-3:] == 'txt':
-        with open('35个转录因子序列集/' + file, 'r') as f:
+        with open('TFs/' + file, 'r') as f:
             lines = f.readlines()
             lines1 = [i for i in lines if i != '\n']
-    # seq = [i.strip() for i in list(set(lines1))[:2622]]#下采样平衡数据
+    # seq = [i.strip() for i in list(set(lines1))[:2622]] #subsampled
     seq = [i.strip() for i in random.sample(list(set(lines1)),2622)]
     data.append(seq)
-    label += [c] * len(seq)#这个是什么
+    label += [c] * len(seq)
     c += 1
 
 
-#截长补短，统一序列长度为800
+#seq=800
 # def pad_seq(seq, maxlen=800):
 #     seq = seq[:maxlen]
 #     return seq + 'N' * (maxlen - len(seq))
@@ -32,7 +32,7 @@ def pad_seq(seq, maxlen=800):
         a=int((len(seq)-800)/2-1)
         l = seq[a:(a+800)]
     return l
-#one hot编码
+#one hot coding
 onehotdict = {
     'A':[1,0,0,0],
     'C':[0,1,0,0],
@@ -47,7 +47,8 @@ def one_hot(seq):
         onehotdata.append(onehotdict[i])
     return np.array(onehotdata)
 data_onehot = []
-#调用上面函数的主函数
+
+
 for i in data:
     for j in range(len(i)):
         i[j] = pad_seq(i[j])
@@ -55,19 +56,21 @@ for i in data:
 data_onehot = np.array(data_onehot)
 label = np.array(label)
 
-#打乱顺序,生成一个储存下标的数组，将其打乱，然后按照这个下标将label和data_onehot的内容都打乱
+#shuffle
 index = [i for i in range(len(data_onehot))]
 np.random.shuffle(index)
 data_onehot = data_onehot[index]
 label = label[index]
 # print(data_onehot)
 # print(label)
-# 分割训练集和测试集
+
+# split data
 train_data = data_onehot[:int(len(data_onehot)*0.8)]
 train_label = label[:int(len(label)*0.8)]
 test_data = data_onehot[int(len(data_onehot)*0.8):]
 test_label = label[int(len(label)*0.8):]
-#保存数据
+
+#save 
 np.save('train_data', train_data)
 np.save('train_label', train_label)
 np.save('test_data', test_data)
